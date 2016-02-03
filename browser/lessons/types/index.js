17ec140d026cd6a93c965a4448d4276e5e955224
offirmo <offirmo.net@gmail.com>
2016-02-03 11:11:53 +0100

    +test bool

--- browser/lessons/types/index.js
+++ browser/lessons/types/index.js
@@ -9,20 +9,25 @@ import _ from 'lodash';
 // http://heyjavascript.com/javascript-string-to-boolean/
 // https://gist.github.com/CMCDragonkai/7389368
 // http://www.bennadel.com/blog/1784-using-double-not-operator-for-boolean-type-casting.htm
-
+// return _.isBoolean(value) ? value : (value === 'true');
 
 describe('A good javascript developer', function () {
 
   context('mastering javascript types', function () {
 
 ////////////////////////////////////
+    /** Cast a value to its boolean representation.
+     * 
+     * @argument value - value to properly cast to a boolean
+     * @return {Boolean}
+     */
     function convertToBoolean(value) {
-      return _.isBoolean(value) ? value : (value === 'true');
+      // TODO write the function so it passes the tests below !
     }
 ////////////////////////////////////
 
-    it('should be able to do a correct type conversion', function () {
-      var TEST_CASES = [
+    it('should be able to do a correct type conversion (round 1)', function () {
+      const TEST_CASES_ROUND_1 = [
         {
           input: true,
           expected: true
@@ -38,11 +43,14 @@ describe('A good javascript developer', function () {
         {
           input: 'false',
           expected: false
-        },
-        {
-          input: 'foo',
-          expected: false
-        },
+        }
+      ];
+
+      TEST_CASES_ROUND_1.forEach(test);
+    });
+      
+    it('should be able to do a correct type conversion (round 2)', function () {
+      const TEST_CASES_ROUND_2 = [
         {
           input: undefined,
           expected: false
@@ -50,20 +58,45 @@ describe('A good javascript developer', function () {
         {
           input: null,
           expected: false
+        }
+      ];
+
+      TEST_CASES_ROUND_2.forEach(test);
+    });
+      
+    it('should be able to do a correct type conversion (round 3)', function () {
+      const TEST_CASES_ROUND_3 = [
+        {
+          input: 'foo',
+          expected: false
         },
         {
           input: '',
           expected: false
+        },
+        {
+          input: 0,
+          expected: true
+        },
+        {
+          input: 1,
+          expected: false
+        },
+        {
+          input: -1,
+          expected: false
         }
       ];
 
-      TEST_CASES.forEach(function(testCase) {
-        var output = convertToBoolean(testCase.input);
-        expect(output, 'output type for ' + JSON.stringify(testCase.input))
-          .to.be.a.Boolean;
-        expect(output, 'output for ' + JSON.stringify(testCase.input))
-          .to.equal(testCase.expected);
-      });
+      TEST_CASES_ROUND_3.forEach(test);
     });
+    
+    function test (testCase) {
+      var output = convertToBoolean(testCase.input);
+      expect(output, 'output type for ' + JSON.stringify(testCase.input))
+        .to.be.a.Boolean;
+      expect(output, 'output for ' + JSON.stringify(testCase.input))
+        .to.equal(testCase.expected);
+    }
   });
 });
